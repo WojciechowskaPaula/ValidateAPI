173795e8b64c4cb4c53cf2e5fba3627eff8c4763
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-07-05 12:24:33 +0200

    Adding ValidateEmail method

---
+++ ValidateAPI/Controllers/ValidateController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ValidateAPI.Helpers;
+
+namespace ValidateAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValidateController : ControllerBase
+    {
+        [HttpGet]
+        [Route("ValidateEmail")]
+        public IActionResult ValidateEmail(string email)
+        {
+           var valid = Email.IsEmailValid(email);
+           if(valid == true)
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
+    }
+}
---
+++ ValidateAPI/Helpers/Email.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Mail;
+
+namespace ValidateAPI.Helpers
+{
+    public static class Email
+    {
+        public static bool IsEmailValid(string email)
+        {
+            var valid = true;
+            try
+            {
+                var emailAdress = new MailAddress(email);
+            }
+            catch
+            {
+                valid = false;
+            }
+            return valid;
+        }
+    }
+}
