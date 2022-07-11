0bab57f843ad504330ba5366fb88f403163324d5
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-07-11 17:42:11 +0200

    Adding unit tests for CheckIsANumber method

---
+++ ValidateAPI.Test/ExtensionsTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using ValidateAPI.Helpers;
+
+namespace ValidateAPI.Test
+{
+    public class ExtensionsTest
+    {
+        [Fact]
+        public void Should_ReturnTrueWhenCharIsADigit()
+        {
+            string phoneNum = "223222122";
+            var result = phoneNum.CheckIsANumber();
+            Assert.True(result);
+        }
+        
+        [Theory]
+        [InlineData("98878oop")]
+        [InlineData("443434343434344566!!")]
+        [InlineData(" ")]
+        [InlineData("e")]
+        public void Should_ReturnFalseWhenPhoneNumberIsNotValid(string phoneNumber)
+        {
+            var result = phoneNumber.CheckIsANumber();
+            Assert.False(result);
+        }
+    }
+}
