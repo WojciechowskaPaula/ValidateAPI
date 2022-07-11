48a4f65b470fca43ce33f5b5b73e70e1d38785c5
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-07-11 18:30:26 +0200

    Adding unit test to ValidatePhoneNumber method

---
+++ ValidateAPI.Test/PhoneNumberTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using ValidateAPI.Helpers;
+
+namespace ValidateAPI.Test
+{
+    public class PhoneNumberTests
+    {
+        [Theory]
+        [InlineData("48996887564")]
+        [InlineData("+48776843904")]
+        [InlineData("887403982")]
+        public void ShouldCheckIsPhoneNumberValid(string phoneNumber)
+        {
+            var result = PhoneNumber.IsPhoneNumberValid(phoneNumber);
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData("557333jj54")]
+        [InlineData("557334000000000000033")]
+        [InlineData("")]
+        [InlineData("> ")]
+        public void ShouldCheckIsPhoneNumberNotValid(string phoneNumber)
+        {
+            var result = PhoneNumber.IsPhoneNumberValid(phoneNumber);
+            Assert.False(result);
+        }
+    }
+}
