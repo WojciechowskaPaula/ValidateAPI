0fd05a854d02533b1af7d444dc0d9c38ad801319
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-07-12 11:07:54 +0200

    Swagger documentation improvement in ValidateController

--- ValidateAPI/Controllers/ValidateController.cs
+++ ValidateAPI/Controllers/ValidateController.cs
@@ -14,8 +14,16 @@ namespace ValidateAPI.Controllers
             _logger = logger;
         }
 
+        /// <summary>
+        /// Check email validation
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns>Boolean data type</returns>
         [HttpGet]
         [Route("ValidateEmail")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public IActionResult ValidateEmail(string email)
         {
             try
@@ -38,8 +46,16 @@ namespace ValidateAPI.Controllers
           
         }
 
+        /// <summary>
+        /// Check phone number validation
+        /// </summary>
+        /// <param name="phoneNumber"></param>
+        /// <returns>Boolean data type</returns>
         [HttpGet]
         [Route("ValidatePhoneNumber")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public IActionResult ValidatePhoneNumber(string phoneNumber)
         {
             try
@@ -60,29 +76,45 @@ namespace ValidateAPI.Controllers
             }
         }
 
+        /// <summary>
+        /// Check length validation
+        /// </summary>
+        /// <param name="word"></param>
+        /// <param name="lenght"></param>
+        /// <returns>Boolean data type</returns>
         [HttpGet]
         [Route("ValidateLength")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public IActionResult ValidateLength(string word, int lenght)
         {
             try
             {
-                _logger.LogInformation($"action=validateLenght word={word}");
+                _logger.LogInformation($"action=validateLength word={word}");
                 var result = Length.IsLengthValid(word, lenght);
                 if (result == true)
                 {
-                    _logger.LogInformation($"action=validateLenght msg='string lenght is valid'");
+                    _logger.LogInformation($"action=validateLength msg='string lenght is valid'");
                     return Ok();
                 }
-                _logger.LogWarning($"action=validateLenght msg='{word} is not valid'");
+                _logger.LogWarning($"action=validateLength msg='{word} is not valid'");
                 return BadRequest();
             }
             catch(Exception ex)
             {
-                _logger.LogError($"action=validateLenght msg='{ex.Message}'", ex);
+                _logger.LogError($"action=validateLength msg='{ex.Message}'", ex);
                 return StatusCode(500);
             }
         }
 
+        /// <summary>
+        /// Check host - HTTP header validation
+        /// </summary>
+        /// <returns>Boolean data type</returns>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [HttpPost]
         [Route("ValidateHostHeader")]
         public IActionResult ValidateHostHeader()
