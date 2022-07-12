4c2f8d67e1229edd45513fb0d57174a9952f8afa
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-07-12 12:47:57 +0200

    adding Swagger configuration

--- ValidateAPI/Program.cs
+++ ValidateAPI/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.OpenApi.Models;
 using Serilog;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +9,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(x=>
+{
+    x.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Validate API",
+        Description = "API for validate parameters",
+        Contact = new OpenApiContact
+        {
+            Name = "Paula Wojciechowska-Rynkowska",
+            Email = "paulla.wojciechowska@gmail.com"
+        }
+    });
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath =(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+    x.IncludeXmlComments(xmlPath);
+});
 builder.Host.UseSerilog((ctx, lc) => lc
 .WriteTo.Console()
 .WriteTo.File(".\\Logs\\ValidateAPI.log.txt", rollingInterval: RollingInterval.Day));
